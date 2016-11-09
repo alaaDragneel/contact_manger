c50d74d50326fa7e089a42de26310afede25eb7a
alaaDragneel <githup/alaaDragneel>
2016-11-09 13:11:45 +0200

    DB

--- contact_manger.sql
+++ contact_manger.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2016 at 06:55 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- Generation Time: Nov 09, 2016 at 12:11 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -37,6 +37,7 @@ CREATE TABLE `contacts` (
   `phone` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -45,22 +46,28 @@ CREATE TABLE `contacts` (
 -- Dumping data for table `contacts`
 --
 
-INSERT INTO `contacts` (`id`, `group_id`, `name`, `company`, `email`, `phone`, `address`, `image`, `created_at`, `updated_at`) VALUES
-(1, 3, 'Jordy Legros', 'Swift-Friesen', 'ndickinson@hotmail.com', '475.279.8000', 'McCullough Spring 42502 New Rashawn', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(2, 2, 'Miss Amber McGlynn', 'VonRueden, Dach and Lakin', 'lindsay.will@gmail.com', '(705) 708-6908 x7552', 'Flatley Stream 30856-8963 Javontemouth', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(3, 1, 'Emmanuel Smitham Sr.', 'Flatley, Schuster and Lueilwitz', 'predovic.jazmin@hotmail.com', '689-836-5583 x6708', 'Brielle Skyway 78494 West Adelatown', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(4, 3, 'Triston Doyle', 'Koepp, Wisozk and Johnston', 'dpredovic@crona.com', '576-200-2365', 'Axel Trace 67771 North Marina', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(5, 3, 'Isabell Stehr', 'Daniel-Hoppe', 'angelica50@boehm.com', '1-360-607-7299 x189', 'Harris Estate 85448 Madilyntown', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(6, 3, 'Marshall Bosco', 'Toy, Reinger and Walker', 'dulce00@hotmail.com', '(465) 800-2651 x13549', 'Towne Terrace 75135-2143 Hauckshire', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(7, 1, 'Brenden Doyle V', 'D''Amore-Hermiston', 'beahan.renee@hotmail.com', '+18546867439', 'Gibson Forges 91648-3031 Lake Jarrett', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(8, 3, 'Dr. Amari Ledner Jr.', 'Marquardt, Mueller and Gutkowski', 'kari.pfeffer@mclaughlin.net', '512-808-3298 x802', 'Tremblay Glens 63792 Adelabury', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(9, 3, 'Nikki Runolfsdottir', 'Ondricka LLC', 'bvon@kessler.com', '828-856-9490 x200', 'Carolyne Turnpike 59681 Franciscoberg', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(10, 1, 'alaa Hauck', 'Bradtke and Sons', 'ferry.kayli@yahoo.com', '669-222-1761', 'Lupe Glens 85894-1254 Strackemouth', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-06 14:41:31'),
-(11, 2, 'Jaclyn Cronin MD', 'Tromp PLC', 'tre.zemlak@oconnell.biz', '1-719-903-8944 x448', 'Lilla Harbor 11814 South Orion', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(12, 1, 'Prof. Horacio Boyle', 'Welch Group', 'clarabelle66@yahoo.com', '274-600-7751 x76618', 'Jaskolski Overpass 54618 Emmanuelville', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(13, 1, 'Shanna Heathcote', 'Prohaska, Ratke and Gerhold', 'maxie.emmerich@emmerich.com', '532.740.7277 x987', 'Pfeffer Mount 88310 Coleview', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(14, 2, 'Ibrahim Rogahn', 'Wunsch PLC', 'christine09@yahoo.com', '907-512-6727 x25015', 'Pollich Centers 17902 Port Kameronton', 'assets/img/avatar5.png', '2016-11-04 12:55:22', '2016-11-04 12:55:22'),
-(15, 1, 'Peter Grant II', 'Jacobi, Abshire and Kutch', 'demetrius.stark@volkman.com', '+1-741-700-8419', 'Evalyn Common 54370-1077 Port Salmashire', 'assets/img/2016-11-04-06-43-48_955c01f49c446bf8617f5d176c33f2202a94f53a.jpg', '2016-11-04 12:55:22', '2016-11-04 16:43:48');
+INSERT INTO `contacts` (`id`, `group_id`, `name`, `company`, `email`, `phone`, `address`, `image`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Jeanette Buckridge', 'Green, Miller and Reichert', 'alaina.boyle@stiedemann.com', '537-273-6415 x51515', 'Lexie Burg 65438 South Cary', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(2, 1, 'Dorothy Bernhard III', 'Bruen, Breitenberg and Senger', 'zane.hammes@hotmail.com', '1-804-842-3597', 'Ashley Drives 06703-4638 West Bud', 'assets/img/avatar5.png', 1, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(3, 3, 'Sanford Casper IV', 'Gleason-Thompson', 'roberto62@gmail.com', '227-926-5478', 'Orn Junctions 20548 Corbinhaven', 'assets/img/avatar5.png', 3, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(4, 2, 'Alexandrine Ruecker DVM', 'Kuhic LLC', 'hessel.jess@witting.com', '398-879-1320', 'Schinner Brook 09359-4167 North Emoryton', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(5, 1, 'Cindy Schuppe I', 'Gerlach LLC', 'marquis41@schmidt.org', '1-539-519-1201 x1080', 'Danielle Underpass 14137-4060 East Estefania', 'assets/img/avatar5.png', 3, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(6, 2, 'Prof. Dallas Pagac', 'Robel Group', 'littel.baby@gerhold.com', '1-657-261-3651', 'Katelin Wells 25037 Baumbachchester', 'assets/img/avatar5.png', 3, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(7, 2, 'Prof. Wendell Bahringer', 'Wolff and Sons', 'rbreitenberg@gmail.com', '1-710-719-6996 x314', 'Karli Fall 64786 Port Curtton', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(8, 3, 'Creola Lindgren V', 'Metz-Collins', 'dibbert.dawson@hessel.com', '812.928.8785 x4409', 'Rogahn Meadow 69393-3460 South Elvis', 'assets/img/avatar5.png', 1, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(9, 1, 'Roxane Swaniawski', 'Kiehn-Greenfelder', 'jbogisich@yahoo.com', '(931) 678-4470', 'Magdalena Wells 65466 Port Mariliehaven', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(10, 1, 'Leatha Flatley', 'Satterfield-Schmeler', 'kling.leonardo@kerluke.com', '896.747.2798', 'Abagail Estates 22858 West Lilian', 'assets/img/avatar5.png', 3, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(11, 3, 'Jazmin Sauer', 'Beer Group', 'kale.weissnat@bernhard.com', '293-320-4184 x8527', 'DuBuque Valleys 15808-8345 Runolfssonmouth', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(12, 2, 'Prof. Percy Roob III', 'Gleichner PLC', 'jo.orn@herzog.com', '1-212-798-4715 x864', 'Veum Junctions 44160-5691 East Candido', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(13, 1, 'Boyd Cronin', 'Wisozk Inc', 'sasha.ohara@yahoo.com', '489.488.9691', 'Heidenreich Street 91785-4918 Shieldshaven', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(14, 1, 'Darron Volkman DVM', 'Beer Group', 'rasheed32@yahoo.com', '557.594.8033', 'Cummerata Cove 06405-7865 East Arthur', 'assets/img/avatar5.png', 3, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(15, 2, 'Karianne Swaniawski', 'Kuphal PLC', 'leann55@hotmail.com', '628-371-3871', 'Lemke Circle 52966 Lake Paula', 'assets/img/avatar5.png', 1, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(16, 3, 'Miss Agustina Terry Jr.', 'Schmitt, Bosco and Mayer', 'suzanne43@strosin.org', '+1 (843) 368-5189', 'Kristian Neck 55338-0216 Geovanymouth', 'assets/img/avatar5.png', 1, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(17, 3, 'Ben Kessler', 'Prohaska LLC', 'reichert.mia@osinski.com', '292-850-8954', 'Noemie Locks 09560-8402 West Antonemouth', 'assets/img/avatar5.png', 3, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(18, 3, 'Ms. Palma Collier', 'Jacobi, Hane and Bosco', 'hlemke@weimann.info', '(508) 693-0780 x49175', 'Mona Burg 98198-4521 Lindville', 'assets/img/avatar5.png', 2, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(19, 2, 'Deanna Johnston', 'Skiles-Kshlerin', 'harvey.alexandrine@ebert.info', '+1 (527) 881-6815', 'Irma Landing 72025-3488 Lake Jaylanhaven', 'assets/img/avatar5.png', 1, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(20, 2, 'Margarita Turcotte', 'Kohler-Bechtelar', 'ndickens@hotmail.com', '+1-425-947-0488', 'Brown Isle 65345 West Alfredville', 'assets/img/avatar5.png', 1, '2016-11-09 07:47:46', '2016-11-09 07:47:46'),
+(21, 2, 'testName', 'testCompany', 'testEmail@email.com', 'testPhone', 'test Adress', 'assets/img/avatar5.png', 3, '2016-11-09 08:35:07', '2016-11-09 08:35:07');
 
 -- --------------------------------------------------------
 
@@ -80,14 +87,9 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Family', '2016-11-04 12:55:21', '2016-11-04 12:55:21'),
-(2, 'Friends', '2016-11-04 12:55:21', '2016-11-04 12:55:21'),
-(3, 'Clients', '2016-11-04 12:55:21', '2016-11-04 12:55:21'),
-(4, 'sexy', '2016-11-06 15:40:41', '2016-11-06 15:40:41'),
-(9, 'phones', '2016-11-06 15:53:04', '2016-11-06 15:53:04'),
-(10, 'work', '2016-11-06 15:54:18', '2016-11-06 15:54:18'),
-(11, 'home', '2016-11-06 15:54:49', '2016-11-06 15:54:49'),
-(12, 'text', '2016-11-06 15:55:17', '2016-11-06 15:55:17');
+(1, 'Family', '2016-11-09 07:47:44', '2016-11-09 07:47:44'),
+(2, 'Friends', '2016-11-09 07:47:44', '2016-11-09 07:47:44'),
+(3, 'Clients', '2016-11-09 07:47:44', '2016-11-09 07:47:44');
 
 -- --------------------------------------------------------
 
@@ -138,6 +140,16 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'user0', 'user0@gmail.com', '$2y$10$McTXRcfgaU.RzBXvhK9VkOVS7W2byQGgv.8WD3BTaCx90.kB/cDfK', 'z1kFW0FoTx020mtpYfgZagF6b7nYI1PnVwCAqQIaHAauabwthv2IzjTaooFm', NULL, '2016-11-09 08:30:50'),
+(2, 'user1', 'user1@gmail.com', '$2y$10$OGe0vF129aal9BVphfhb5ejILKUskNvqeyVncyn1kwyRo49/uLtp2', NULL, NULL, NULL),
+(3, 'user2', 'user2@gmail.com', '$2y$10$1KZHDxj8b8jd4FQ5szQZAOyapciYbnZOjQlkdBx/ST0IBpBWCT54q', 'W3K5VnQsPx0Jcy1Fdjtyx6blUgVedkCJo9o2bMPLFObuzFrec48ImLtPyfFk', NULL, '2016-11-09 08:59:23'),
+(4, 'user3', 'user3@gmail.com', '$2y$10$BgEnCtefcAMWXbzTHr6QgOIptgaz7U0c0KrmoUh2AO1gIQgRFXQ4C', NULL, NULL, NULL);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -176,17 +188,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
